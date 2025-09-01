a426343246a479c8c81078198c6ded2684a3b097
Chinaglia <156460261+chinagliadev@users.noreply.github.com>
2025-09-01 10:51:23 -0300

    inserindo banco de dados com php no banco

---
+++ salvarPersonagem.php
@@ -0,0 +1,35 @@
+<?php
+
+$dsn = 'mysql:host=127.0.0.1;dbname=rick_and_morty';
+$usuario = 'root';
+$senha = '';
+
+$conn = new PDO($dsn, $usuario, $senha);
+
+$dadosJson = file_get_contents('php://input');
+$dados = json_decode($dadosJson, true);
+
+if (!$dados) {
+    http_response_code(400);
+    echo "Dados inválidos.";
+}
+
+$id = $dados['id'];
+$nome = $dados['nome'];
+$image = $dados['imagem'];
+
+$stmt = $conn->prepare("SELECT * FROM personagem WHERE id = ?");
+$stmt->execute([$id]);
+
+if ($stmt->rowCount() > 0) {
+    echo "Personagem já salvo.";
+} else {
+    $insert = $conn->prepare("INSERT INTO personagem (id, nome, imagem) VALUES (:id, :nome, :imagem)");
+    $insert->execute([
+        ':id' => $id,
+        ':nome' => $nome,
+        ':imagem' => $image
+    ]);
+    echo "Personagem salvo com sucesso!";
+}
+
---
+++ script.js
@@ -0,0 +1,112 @@
+const containerRick = document.getElementById('rick-container')
+const txtPersonagem = document.getElementById('txtPersonagem')
+const btnPesquisar = document.getElementById('btnPesquisar')
+
+const container = document.getElementById('pesquisar-container')
+
+
+function procurarPersonagem(userID) {
+    fetch("https://rickandmortyapi.com/api/character/" + userID)
+    return fetch("https://rickandmortyapi.com/api/character/" + userID)
+        .then(function (resposta) {
+            return resposta.json()
+        })
+}
+
+async function carregarPersonagens() {
+    for (let i = 1; i <= 30; i++) {
+        const personagem = await procurarPersonagem(i);
+         let idPersonagem = personagem.id
+
+        const coluna = document.createElement('div');
+        coluna.className = 'col';
+
+        // coluna.innerHTML = `
+        //     <div class="card h-100 shadow-sm">
+        //         <div class="card-body text-center">
+        //             <img src="${personagem.image}" alt="${personagem.name}" class="personagem-img mb-2 img-fluid rounded">
+        //             <h5 class="card-title">${personagem.name}</h5>
+        //             <p class="card-text"><strong>Espécie:</strong> ${personagem.species}</p>
+        //             <button class="btn btn-success salvar-btn" 
+        //                 data-id="${personagem.id}" 
+        //                 data-nome="${personagem.name}" 
+        //                 data-imagem="${personagem.image}">
+        //                 Salvar Personagem
+        //             </button>
+        //         </div>
+        //         <div class="card-footer text-center">
+        //             <p class="card-text text-muted"><strong>ID #</strong>${personagem.id}</p>
+        //         </div>
+        //     </div>
+        // `;
+
+                coluna.innerHTML = `
+            <div class="card h-100 shadow-sm">
+                <div class="card-body text-center">
+                    <img src="${personagem.image}" alt="${personagem.name}" class="personagem-img mb-2 img-fluid rounded">
+                    <h5 class="card-title">${personagem.name}</h5>
+                    <p class="card-text"><strong>Espécie:</strong> ${personagem.species}</p>
+                    <a class="btn btn-success" href="./exibirPersonagem.html?id=${idPersonagem}">Ver mais
+                    </a>
+                </div>
+                <div class="card-footer text-center">
+                    <p class="card-text text-muted"><strong>ID #</strong>${personagem.id}</p>
+                </div>
+            </div>
+        `;
+        containerRick.appendChild(coluna);
+    }
+
+    document.querySelectorAll('.salvar-btn').forEach(btn => {
+        btn.addEventListener('click', async function () {
+            const personagem = {
+                id: this.dataset.id,
+                nome: this.dataset.nome,
+                imagem: this.dataset.imagem
+            };
+
+            const resposta = await fetch('./salvarPersonagem.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(personagem)
+            });
+
+            const resultado = await resposta.text();
+            alert(resultado); 
+        });
+    });
+}
+
+async function listarPersonagem() {
+            let valorCampo = txtPersonagem.value
+            const personagem = await procurarPersonagem(valorCampo)
+            let nomePersonagem = personagem.name
+            let especiaPersonagem = personagem.species
+            let idPersonagem = personagem.id
+            let imagemPersonagem = personagem.image
+
+
+            console.log(nomePersonagem)
+
+            const coluna = document.createElement('div')
+            coluna.className = 'd-flex justify-content-center'
+            coluna.innerHTML = `
+            <div class="card h-100 shadow-sm">
+                <div class="card-body text-center">
+                    <img src="${imagemPersonagem}" alt="${nomePersonagem}" class="personagem-img mb-2 img-fluid rounded">
+                    <h5 class="card-tile">${nomePersonagem}</h5>
+                    <p class="card-text"><strong>Espécie: </strong> ${especiaPersonagem}</p>
+                </div>
+                <div class="card-footer text-center"> 
+                    <p class="card-text text-muted"><strong>Id #</strong>${idPersonagem}</p>
+                </div>
+            </div>
+            `
+            container.appendChild(coluna)
+            txtPersonagem.value = ""
+}
+
+
+carregarPersonagens()
